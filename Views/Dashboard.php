e18ec95ecfa71d4b3186ef42b40ce507a03d027d
hossamAhmedSalah <100955137+hossamAhmedSalah@users.noreply.github.com>
2023-11-25 23:44:41 +0200

    dashboard top sales and key figures added

--- Views/Dashboard.php
+++ Views/Dashboard.php
@@ -2,14 +2,24 @@
 require_once './../Shared/Links.php';
 require_once './../Shared/Header.php';
 require_once './../Shared/Sidebar.php';
+require_once __DIR__ . "../../Models/DBManager.php";
 ?>
 
 <main id="main" class="main">
 
     <div class="pagetitle">
-        <h1>Report</h1>
+        <h1>Key Figures</h1>
     </div>
     <!-- End Page Title -->
+    <?php
+    $db = DBManager::getInstance();
+    $countOfProducts = $db->apply_function_to_column("COUNT", "Products", "productID");
+    $sales = $db->apply_function_to_column("SUM","Orders", "totalAmount");
+    $custs = $db->apply_function_to_column('COUNT','Users', "userName", "role", "customer");
+    $limitTopSales = 5;
+    $prods= $db->select("SELECT p.productID, p.productName, p.price, p.stockQuantity, p.numSales, p.categoryID, p.imgURL, c.categoryName FROM Products p JOIN Categories c ON p.categoryID = c.categoryID ORDER BY p.numSales DESC LIMIT ". $limitTopSales);
+
+    ?>
 
     <section class="section dashboard">
         <div class="row">
@@ -22,14 +32,14 @@ require_once './../Shared/Sidebar.php';
                         <div class="card info-card sales-card">
 
                             <div class="card-body">
-                                <h5 class="card-title">Number Of Orders</h5>
+                                <h5 class="card-title">Number Of Prouducts</h5>
 
                                 <div class="d-flex align-items-center">
                                     <div class="card-icon rounded-circle d-flex align-items-center justify-content-center">
                                         <i class="fa-solid fa-cart-shopping"></i>
                                     </div>
                                     <div class="ps-3">
-                                        <h6>145</h6>
+                                        <h6><?php echo $countOfProducts ?></h6>
                                     </div>
                                 </div>
                             </div>
@@ -44,14 +54,14 @@ require_once './../Shared/Sidebar.php';
 
 
                             <div class="card-body">
-                                <h5 class="card-title">Total Money</h5>
+                                <h5 class="card-title">Total Sales</h5>
 
                                 <div class="d-flex align-items-center">
                                     <div class="card-icon rounded-circle d-flex align-items-center justify-content-center">
                                         <i class="fa-solid fa-dollar-sign"></i>
                                     </div>
                                     <div class="ps-3">
-                                        <h6>$3,264</h6>
+                                        <h6>$ <?php echo isset($sales)? $sales:0; ?></h6>
                                     </div>
                                 </div>
                             </div>
@@ -75,7 +85,7 @@ require_once './../Shared/Sidebar.php';
                                         <i class="fa-solid fa-user"></i>
                                     </div>
                                     <div class="ps-3">
-                                        <h6>1244</h6>
+                                        <h6><?php echo $custs ?></h6>
                                     </div>
                                 </div>
 
@@ -94,48 +104,47 @@ require_once './../Shared/Sidebar.php';
                                 <h5 class="card-title">Top Selling</h5>
 
                                 <table class="table table-border">
-                                    <thead>
+                                <thead>
                                         <tr>
+                                            <th>Product ID</th>
                                             <th>Preview</th>
                                             <th>Product</th>
                                             <th>Price</th>
-                                            <th>Sold</th>
+                                            <th>Stock</th>
+                                            <th>Sales</th>
+                                            <th>Category</th>
+                                            <th>Edit</th>
                                         </tr>
                                     </thead>
-                                    <tbody>
-                                        <tr>
-                                            <th><span><img src="../assets/img/product-1.jpg" alt=""></span></th>
-                                            <td><span class="fw-bold">Ut inventore ipsa voluptas nulla</span></td>
-                                            <td>$64</td>
-                                            <td class="fw-bold">124</td>
-                                        </tr>
-                                        <tr>
-                                            <th><span><img src="../assets/img/product-2.jpg" alt=""></span></th>
-                                            <td><span class="fw-bold">Exercitationem similique doloremque</span></td>
-                                            <td>$46</td>
-                                            <td class="fw-bold">98</td>
-                                        </tr>
-                                        <tr>
-                                            <th><span><img src="../assets/img/product-3.jpg" alt=""></span></th>
-                                            <td><span class="fw-bold">Doloribus nisi exercitationem</span></td>
-                                            <td>$59</td>
-                                            <td class="fw-bold">74</td>
-                                        </tr>
-                                        <tr>
-                                            <th><span><img src="../assets/img/product-4.jpg" alt=""></span></th>
-                                            <td><span class="fw-bold">Officiis quaerat sint rerum error</span></td>
-                                            <td>$32</td>
-                                            <td class="fw-bold">63</td>
-                                        </tr>
-                                        <tr>
-                                            <th><span><img src="../assets/img/product-5.jpg" alt=""></span></th>
-                                            <td><span class="fw-bold">Sit unde debitis delectus repellendus</span></td>
-                                            <td>$79</td>
-                                            <td class="fw-bold">41</td>
-                                        </tr>
-                                    </tbody>
-                                </table>
+                                    <?php
+                                    # helper function to chooce unique color for each category
+                                    function getCategoryColor($categoryName)
+                                    {
+                                        $hash = md5($categoryName);
+                                        return '#' . substr($hash, 0, 6);
+                                    }
+                                    ?>
+                                    <?php foreach ($prods as $row) : ?>
 
+                                        <tbody>
+                                            <tr>
+                                                <td><span class="fw-bold"><?php echo $row['productID']; ?></span></td>
+                                                <th><span><img src="<?php echo $row['imgURL']; ?>" alt=""></span></th>
+                                                <td><span class="fw-bold"><?php echo $row['productName']; ?></span></td>
+                                                <td><?php echo $row['price']; ?></td>
+                                                <td><?php echo $row['stockQuantity']; ?></td>
+                                                <td class="fw-bold"><?php echo $row['numSales']; ?></td>
+                                                <td>
+
+                                                    <div style="background-color: <?php echo getCategoryColor($row['categoryName']); ?>; padding: 5px; border-radius: 5px;"><?php echo $row['categoryName']; ?></div>
+                                                </td>
+                                                <td>
+                                                    <button class="btn btn-info"><i class="fa-solid fa-pen-to-square"></i></button>
+                                                </td>
+                                            </tr>
+                                        </tbody>
+                                    <?php endforeach; ?>
+                                </table>
                             </div>
 
                         </div>
