0f43267cd1b5da3b867e5e3af657106edb7a2867
hazem <haazm.994@gmail.com>
2023-11-26 22:05:11 +0200

    ordersDetails Done

--- Views/Orders.php
+++ Views/Orders.php
@@ -2,10 +2,9 @@
 require_once './../Shared/Links.php';
 require_once './../Shared/Header.php';
 require_once './../Shared/Sidebar.php';
+require_once __DIR__ . "../../Models/DBManager.php";
+
 ?>
-
-
-
 
 <main id="main" class="main">
 
@@ -19,52 +18,42 @@ require_once './../Shared/Sidebar.php';
             <div class="col-lg-12">
                 <div class="row">
                     <!-- Primary Color Bordered Table -->
-                    <table class="table table-bordered border-colored shadow">
+                    <table class="table table-bordered border-colored shadow text-center">
                         <thead>
                             <tr>
-                                <th>#</th>
-                                <th>Name</th>
-                                <th>Position</th>
-                                <th>Age</th>
-                                <th>Start Date</th>
+                                <th>Order ID</th>
+                                <th>Customer ID</th>
+                                <th>Total Amount</th>
+                                <th>Address</th>
+                                <th>Address 2</th>
+                                <th>Phone</th>
+                                <th>Email</th>
+                                <th>Date</th>
                             </tr>
                         </thead>
                         <tbody>
-                            <tr>
-                                <th>1</th>
-                                <td>Brandon Jacob</td>
-                                <td>Designer</td>
-                                <td>28</td>
-                                <td>2016-05-25</td>
-                            </tr>
-                            <tr>
-                                <th>2</th>
-                                <td>Bridie Kessler</td>
-                                <td>Developer</td>
-                                <td>35</td>
-                                <td>2014-12-05</td>
-                            </tr>
-                            <tr>
-                                <th>3</th>
-                                <td>Ashleigh Langosh</td>
-                                <td>Finance</td>
-                                <td>45</td>
-                                <td>2011-08-12</td>
-                            </tr>
-                            <tr>
-                                <th>4</th>
-                                <td>Angus Grady</td>
-                                <td>HR</td>
-                                <td>34</td>
-                                <td>2012-06-11</td>
-                            </tr>
-                            <tr>
-                                <th>5</th>
-                                <td>Raheem Lehner</td>
-                                <td>Dynamic Division Officer</td>
-                                <td>47</td>
-                                <td>2011-04-19</td>
-                            </tr>
+                            <?php
+                            $db = DBManager::getInstance();
+                            $Orders_details = $db->select("SELECT * FROM Orders");
+
+                            if ($Orders_details) {
+                                foreach ($Orders_details as $data) {
+                                    echo '<tr>';
+                                    echo '<th>' . $data['orderID'] . '</th>';
+                                    echo '<th>' . $data['userID'] . '</th>';
+                                    echo '<th>' . $data['totalAmount'] . '</th>';
+                                    echo '<th>' . $data['address1'] . '</th>';
+                                    echo '<th>' . $data['address2'] . '</th>';
+                                    echo '<th>' . $data['additionalPhone'] . '</th>';
+                                    echo '<th>' . $data['additionalEmail'] . '</th>';
+                                    echo '<th>' . $data['orderDate'] . '</th>';
+                                    echo '</tr>';
+                                }
+                            } else {
+                                
+                                echo '<tr><td colspan="8" class="alert alert-info" >No orders found</td></tr>';
+                            }
+                            ?>
                         </tbody>
                     </table>
                 </div>
@@ -75,31 +64,8 @@ require_once './../Shared/Sidebar.php';
 
 </main><!-- End #main -->
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 <script src="../assets/js/main.js"></script>
 
-
 <?php
-
 include_once './../Shared/Footer.php';
-
 ?>
