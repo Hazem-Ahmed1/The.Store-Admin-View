6e7b589f23193dc95a8264c590efb26a1cd8014a
hossamAhmedSalah <100955137+hossamAhmedSalah@users.noreply.github.com>
2023-11-26 15:32:45 +0200

    some work on the product page

--- Views/Products.php
+++ Views/Products.php
@@ -33,6 +33,7 @@ require_once __DIR__ . "../../Models/DBManager.php";
                                     $db = DBManager::getInstance();
                                     # An INNER JOIN returns only the rows where there is a match in both tables based on the specified condition
                                     $prods = $db->select("SELECT p.productID, p.productName, p.price, p.stockQuantity, p.numSales, p.categoryID, p.imgURL, c.categoryName FROM Products p JOIN Categories c ON p.categoryID = c.categoryID");
+                                    $cats = $db->select("SELECT * FROM Categories");
                                     ?>
                                     <thead>
                                         <tr>
@@ -88,20 +89,37 @@ require_once __DIR__ . "../../Models/DBManager.php";
 
                 <div class="card">
                     <div class="card-body">
-                        <h5 class="card-title">Add a new category</h5>
-                        <form class="row g-3">
+                        <h5 class="card-title">Add a new Product</h5>
+                        <form class="row g-3" >
                             <div class="col-12">
-                                <label for="cat_name" class="form-label">Category Name</label>
-                                <input type="text" class="form-control" id="cat_name">
+                                <label for="productName" class="form-label">Product Name</label>
+                                <input type="text" class="form-control" id="productName" name="productName" required>
                             </div>
                             <div class="col-12">
-                                <label for="img_url" class="form-label">Image Url</label>
-                                <input type="email" class="form-control" id="img_url">
+                                <label for="description" class="form-label">Description</label>
+                                <input type="text" class="form-control" id="description" name="description" minlength="30" required>
+                            </div>
+                            <div class="col-12">
+                                <label for="price" class="form-label">Price</label>
+                                <input type="number" class="form-control" id="price" name="price" required min="1">
+                            </div>
+                            <div class="col-12">
+                                <label for="stockQuantity" class="form-label">Stock Quantity</label>
+                                <input type="number" class="form-control" id="stockQuantity" name="stockQuantity" min="1" required>
+                            </div>
+                            <div class="col-12">
+                                <label for="categoryID" class="form-label">Category</label>
+                                <select class="form-select" id="categoryID" name="categoryID">
+                                    <?php foreach($cats as $cat) : ?>
+                                    <!-- filling this list dynamically from available categories in the database -->
+                                    <option value="<?php echo $cat["categoryID"];?>" style="font-size: 30;"><?php echo $cat['categoryName']; ?></option>
+                                    <?php endforeach;?>
+                                </select>
                             </div>
                             <div class="text-center">
-                                <button type="submit" class="col-12 btn btn-primary">Submit</button>
+                                <button type="submit" class="col-12 btn btn-primary">Add New Product</button>
                             </div>
-                        </form><!-- Vertical Form -->
+                        </form> <!-- form end-->
 
                     </div>
                 </div>
